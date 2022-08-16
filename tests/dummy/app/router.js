6d15f4d9340ee3b61e6e30cab713cabd6d5be7ef
Matvienko Oleg <OMatvienko@ics.perm.ru>
2022-08-16 17:43:08 +0500

    Update to 3.1.4

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,9 +1,10 @@
-import Ember from 'ember';
+import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
 
 const Router = Ember.Router.extend({
-  location: config.locationType
+  location: config.locationType,
+  rootURL: config.rootURL
 });
 
 Router.map(function() {
